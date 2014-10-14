859200233d9ec743f9b8d663cb8d400318e932f5
Denis Papathanasiou <denis@papathanasiou.org>
2014-10-14 16:30:26 -0400

    Added db support for books: barcodes correspond to isbn-10 or isbn-13, and the initial data to be provided by parsing the open library dumps

--- server/database/books.sql
+++ server/database/books.sql
@@ -8,6 +8,38 @@
 -- source: http://stackoverflow.com/a/10951183
 
 
--- (books are in their own universe)
--- so, later, when find good isbn source, add tables for:
--- BOOK, AUTHOR, BOOK_AUTHORS
+-- `book` defines basic title and barcode (isbn) information, and the
+-- corresponding source
+
+CREATE TABLE book (
+	id        binary(16) primary key NOT NULL,
+	title     varchar(1024) NOT NULL,
+	isbn      varchar(13) NOT NULL,
+	is_isbn10 boolean DEFAULT false,
+	src       varchar(32) NOT NULL DEFAULT 'OL', -- open library
+	posted    datetime, -- automatically filled in by trigger, below
+	UNIQUE(title, isbn, src) -- an isbn *should* be unique but sources may differ
+);
+
+CREATE TRIGGER book_on_insert BEFORE INSERT ON `book`
+    FOR EACH ROW SET NEW.posted = IFNULL(NEW.posted, NOW());
+
+
+-- `author` defines author names, and the corresponding source information
+
+CREATE TABLE author (
+	id        binary(16) primary key NOT NULL,
+	full_name varchar(1024) NOT NULL,
+	src       varchar(32) NOT NULL DEFAULT 'OL', -- open library
+	src_id    varchar(64) -- a unique identifier from the source (if present)
+);
+
+
+-- `book_author` defines which author(s) are credited with which books
+
+CREATE TABLE book_author (
+	id        binary(16) primary key NOT NULL,
+	book_id   binary(16) references book(id),
+	author_id binary(16) references author(id),
+	UNIQUE(book_id, author_id)
+);
