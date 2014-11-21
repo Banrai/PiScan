6db56a7a29642bc16eebc650a0206abaf7000ffa
Denis Papathanasiou <denis@papathanasiou.org>
2014-11-21 17:46:18 -0500

    Start of the (remote) api server, which runs the POD db clone, and takes barcode lookup requests from the Pi clients

---
+++ server/api/server.go
@@ -0,0 +1,98 @@
+// Copyright Banrai LLC. All rights reserved. Use of this source code is
+// governed by the license that can be found in the LICENSE file.
+
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/Banrai/PiScan/server/database/barcodes"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Server struct {
+	mux    *http.ServeMux
+	s      *http.Server
+	Logger *log.Logger
+}
+
+var (
+	Srv                      *Server
+	DefaultServerReadTimeout = 30 // in seconds
+)
+
+func InitServerDatabase(dbUser, dbPass, dbHost string, dbPort int) map[string]*sql.Stmt {
+	statements := map[string]*sql.Stmt{}
+
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%d)/product_open_data", dbUser, dbPass, dbHost, dbPort)
+	db, err := sql.Open("mysql", connection)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	gtin, err := db.Prepare(barcodes.GTIN_LOOKUP)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		statements[barcodes.GTIN_LOOKUP] = gtin
+	}
+	defer gtin.Close()
+
+	brand, err := db.Prepare(barcodes.BRAND_LOOKUP)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		statements[barcodes.BRAND_LOOKUP] = brand
+	}
+	defer brand.Close()
+
+	asinLookup, err := db.Prepare(barcodes.ASIN_LOOKUP)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		statements[barcodes.ASIN_LOOKUP] = asinLookup
+	}
+	defer asinLookup.Close()
+
+	asinInsert, err := db.Prepare(barcodes.ASIN_INSERT)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		statements[barcodes.ASIN_INSERT] = asinInsert
+	}
+	defer asinInsert.Close()
+
+	return statements
+}
+
+func Respond(mediaType string, charset string, fn func(w http.ResponseWriter, r *http.Request) string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", fmt.Sprintf("%s; charset=%s", mediaType, charset))
+		data := fn(w, r)
+		w.Header().Set("Content-Length", fmt.Sprintf("%d", len(data)))
+		fmt.Fprintf(w, data)
+	}
+}
+
+func NewAPIServer(host string, port int, timeout int, handlers map[string]func(http.ResponseWriter, *http.Request)) {
+	mux := http.NewServeMux()
+	for pattern, handler := range handlers {
+		mux.Handle(pattern, http.HandlerFunc(handler))
+	}
+	s := &http.Server{
+		Addr:        fmt.Sprintf("%s:%d", host, port),
+		Handler:     mux,
+		ReadTimeout: time.Duration(timeout) * time.Second, // to prevent abuse of "keep-alive" requests by clients
+	}
+	Srv = &Server{
+		mux:    mux,
+		s:      s,
+		Logger: log.New(os.Stdout, "", log.Ldate|log.Ltime),
+	}
+	Srv.s.ListenAndServe()
+}
---
+++ server/main.go
@@ -0,0 +1,86 @@
+// Copyright Banrai LLC. All rights reserved. Use of this source code is
+// governed by the license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/Banrai/PiScan/server/api"
+	"github.com/Banrai/PiScan/server/commerce"
+	"github.com/Banrai/PiScan/server/commerce/amazon"
+	"github.com/Banrai/PiScan/server/database/barcodes"
+	"net/http"
+	"strings"
+)
+
+const (
+	// API server definitions
+	apiServer = ""
+	apiPort   = 9001
+
+	// Coordinates for the POD db clone (mysql) on this server
+	barcodeDBUser   = "pod"
+	barcodeDBPass   = ""
+	barcodeDBServer = "127.0.0.1"
+	barcodeDBPort   = 3306
+)
+
+// Lookup the barcode, using both the barcodes database, and the Amazon API
+func lookupBarcode(r *http.Request, statements map[string]*sql.Stmt) string {
+	// the result is a json representation of the list of found products
+	products := make([]*commerce.API, 0)
+
+	// this function only responds to POST requests
+	if "POST" == r.Method {
+		r.ParseForm()
+
+		barcodeVal, barcodeExists := r.PostForm["barcode"]
+		if barcodeExists {
+			asinLookup, asinLookupExists := statements[barcodes.ASIN_LOOKUP]
+			asinInsert, asinInsertExists := statements[barcodes.ASIN_INSERT]
+			if asinLookupExists && asinInsertExists {
+				prods, prodErr := amazon.Lookup(strings.Join(barcodeVal, ""), asinLookup, asinInsert)
+				if prodErr == nil {
+					for _, prod := range prods {
+						products = append(products, prod)
+					}
+				}
+			}
+		}
+	}
+
+	result, err := json.Marshal(products)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return string(result)
+}
+
+func main() {
+	var (
+		dbUser, dbPass, dbHost, host string
+		dbPort, port                 int
+	)
+
+	flag.StringVar(&dbUser, "dbUser", barcodeDBUser, fmt.Sprintf("The barcodes database user (defaults to '%s')", barcodeDBUser))
+	flag.StringVar(&dbPass, "dbPass", barcodeDBPass, fmt.Sprintf("The barcodes database password (defaults to '%s')", barcodeDBPass))
+	flag.StringVar(&dbHost, "dbHost", barcodeDBServer, fmt.Sprintf("The barcodes database server (defaults to '%s')", barcodeDBServer))
+	flag.IntVar(&dbPort, "dbPort", barcodeDBPort, fmt.Sprintf("The barcodes database port (defaults to '%d')", barcodeDBPort))
+	flag.StringVar(&host, "host", apiServer, fmt.Sprintf("The hostname or IP address of the API server (defaults to '%s')", apiServer))
+	flag.IntVar(&port, "port", port, fmt.Sprintf("The API server port (defaults to '%d')", port))
+	flag.Parse()
+
+	handlers := map[string]func(http.ResponseWriter, *http.Request){}
+	handlers["/lookup"] = func(w http.ResponseWriter, r *http.Request) {
+		lookup := func(w http.ResponseWriter, r *http.Request) string {
+			statements := api.InitServerDatabase(dbUser, dbPass, dbHost, dbPort)
+			return lookupBarcode(r, statements)
+		}
+		api.Respond("application/json", "utf-8", lookup)(w, r)
+	}
+	api.NewAPIServer(host, port, api.DefaultServerReadTimeout, handlers)
+
+}
