4634908d4e6b4fbbc4fe37269883dbadfcda75ef
Denis Papathanasiou <denis@papathanasiou.org>
2014-11-22 16:18:34 -0500

    Switched the client scanner app to use the API server for barcode lookups

--- client/scanner.go
+++ client/scanner.go
@@ -6,30 +6,32 @@
 package main
 
 import (
-	"database/sql"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/Banrai/PiScan/client/database"
 	"github.com/Banrai/PiScan/scanner"
-	"github.com/Banrai/PiScan/server/commerce/amazon"
-	"github.com/Banrai/PiScan/server/database/barcodes"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/Banrai/PiScan/server/commerce"
+	"io/ioutil"
 	"log"
-	"strings"
+	"net/http"
+	"net/url"
 )
 
-const ( // barcodes db lookups will be via internal api to a remote server, eventually
-	barcodeDBUser   = "pod"
-	barcodeDBServer = "127.0.0.1"
-	barcodeDBPort   = "3306"
+const (
+	apiServerHost = "http://saruzai.com"
+	apiServerPort = 9001
 )
 
 func main() {
-	var device, dbUser, dbHost, dbPort, sqlitePath, sqliteFile, sqliteTablesDefinitionPath string
+	var (
+		device, apiServer, sqlitePath, sqliteFile, sqliteTablesDefinitionPath string
+		apiPort                                                               int
+	)
+
 	flag.StringVar(&device, "device", scanner.SCANNER_DEVICE, fmt.Sprintf("The '/dev/input/event' device associated with your scanner (defaults to '%s')", scanner.SCANNER_DEVICE))
-	flag.StringVar(&dbUser, "dbUser", barcodeDBUser, fmt.Sprintf("The barcodes database user (defaults to '%s')", barcodeDBUser))
-	flag.StringVar(&dbHost, "dbHost", barcodeDBServer, fmt.Sprintf("The barcodes database server (defaults to '%s')", barcodeDBServer))
-	flag.StringVar(&dbPort, "dbPort", barcodeDBPort, fmt.Sprintf("The barcodes database port (defaults to '%s')", barcodeDBPort))
+	flag.StringVar(&apiServer, "apiHost", apiServerHost, fmt.Sprintf("The hostname or IP address of the API server (defaults to '%s')", apiServerHost))
+	flag.IntVar(&apiPort, "apiPort", apiServerPort, fmt.Sprintf("The API server port (defaults to '%d')", apiServerPort))
 	flag.StringVar(&sqlitePath, "sqlitePath", database.SQLITE_PATH, fmt.Sprintf("Path to the sqlite file (defaults to '%s')", database.SQLITE_PATH))
 	flag.StringVar(&sqliteFile, "sqliteFile", database.SQLITE_FILE, fmt.Sprintf("The sqlite database file (defaults to '%s')", database.SQLITE_FILE))
 	flag.StringVar(&sqliteTablesDefinitionPath, "sqliteTables", "", fmt.Sprintf("Path to the sqlite database definitions file, %s, (use only if creating the client db for the first time)", database.TABLE_SQL_DEFINITIONS))
@@ -46,110 +48,88 @@ func main() {
 	} else {
 		// a regular scanner processing event
 
-		// eventually, make all this via an internal api call to a remote server, but for now...
-		// connect to the barcodes database and make all the prepared statements available to scanner functions
-		db, err := sql.Open("mysql",
-			strings.Join([]string{dbUser, "@tcp(", dbHost, ":", dbPort, ")/product_open_data"}, ""))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer db.Close()
-
-		gtin, err := db.Prepare(barcodes.GTIN_LOOKUP)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer gtin.Close()
-
-		brand, err := db.Prepare(barcodes.BRAND_LOOKUP)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer brand.Close()
-
-		asinLookup, err := db.Prepare(barcodes.ASIN_LOOKUP)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer asinLookup.Close()
-
-		asinInsert, err := db.Prepare(barcodes.ASIN_INSERT)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer asinInsert.Close()
-
 		// coordinates for connecting to the sqlite database (from the command line options)
 		dbCoordinates := database.ConnCoordinates{DBPath: sqlitePath, DBFile: sqliteFile}
 
 		processScanFn := func(barcode string) {
-			// Lookup the barcode, using both the barcodes database, and the Amazon API
-			products, err := amazon.Lookup(barcode, asinLookup, asinInsert)
+			// Lookup the barcode in the API server
+			apiResponse, apiErr := http.PostForm(fmt.Sprintf("%s:%d/lookup", apiServer, apiPort), url.Values{"barcode": {barcode}})
+			if apiErr != nil {
+				fmt.Println(fmt.Sprintf("API access error: %s", apiErr))
+				return
+			}
+			rawJson, _ := ioutil.ReadAll(apiResponse.Body)
+			apiResponse.Body.Close()
+
+			var products []*commerce.API
+			err := json.Unmarshal(rawJson, &products)
 			if err != nil {
-				fmt.Println(fmt.Sprintf("Barcode lookup error: %s", err))
-			} else {
-				// attempt to connect to the sqlite db
-				db, dbErr := database.InitializeDB(dbCoordinates)
-				if err != nil {
-					fmt.Println(fmt.Sprintf("Client db access error: %s", dbErr))
-					return
-				}
-				defer db.Close()
+				fmt.Println(fmt.Sprintf("API barcode lookup error: %s", err))
+				return
+			}
 
-				// get the Account for this request
-				acc, accErr := database.GetDesignatedAccount(db)
-				if accErr != nil {
-					fmt.Println(fmt.Sprintf("Client db account access error: %s", accErr))
-					return
-				}
+			// attempt to connect to the sqlite db
+			db, dbErr := database.InitializeDB(dbCoordinates)
+			if err != nil {
+				fmt.Println(fmt.Sprintf("Client db access error: %s", dbErr))
+				return
+			}
+			defer db.Close()
 
-				// get the list of current Vendors according to the Pi client database
-				// and map them according to their API vendor id string
-				vendors := make(map[string]*database.Vendor)
-				for _, v := range database.GetAllVendors(db) {
-					vendors[v.VendorId] = v
-				}
+			// get the Account for this request
+			acc, accErr := database.GetDesignatedAccount(db)
+			if accErr != nil {
+				fmt.Println(fmt.Sprintf("Client db account access error: %s", accErr))
+				return
+			}
 
-				productsFound := 0
-				for i, product := range products {
-					//fmt.Println(fmt.Sprintf("(%d) SKU %s Name %s Type %s Vendor %s", i, product.SKU, product.ProductName, product.ProductType, product.Vendor))
-					v, exists := vendors[product.Vendor]
-					if !exists {
-						amazonId, amazonErr := database.AddVendor(db, product.Vendor, "Amazon")
-						if amazonErr == nil {
-							v = database.GetVendor(db, amazonId)
-							vendors[product.Vendor] = v
-						}
-					}
+			// get the list of current Vendors according to the Pi client database
+			// and map them according to their API vendor id string
+			vendors := make(map[string]*database.Vendor)
+			for _, v := range database.GetAllVendors(db) {
+				vendors[v.VendorId] = v
+			}
 
-					if len(product.ProductName) > 0 {
-						// convert the commerce.API struct into a database.Item
-						// so that it can be logged into the Pi client sqlite db
-						item := database.Item{
-							Index:           int64(i),
-							Barcode:         barcode,
-							Desc:            product.ProductName,
-							UserContributed: false}
-						pk, insertErr := item.Add(db, acc)
-						if insertErr == nil {
-							// also log the vendor/product code combination
-							database.AddVendorProduct(db, product.SKU, v.Id, pk)
-						}
-						productsFound += 1
+			productsFound := 0
+			for i, product := range products {
+				v, exists := vendors[product.Vendor]
+				if !exists {
+					amazonId, amazonErr := database.AddVendor(db, product.Vendor, "Amazon")
+					if amazonErr == nil {
+						v = database.GetVendor(db, amazonId)
+						vendors[product.Vendor] = v
 					}
 				}
 
-				if productsFound == 0 {
-					// add it to the Pi client sqlite db as "unknown"
-					// so that it can be manually edited/input
-					unknownItem := database.Item{Index: 0, Barcode: barcode}
-					unknownItem.Add(db, acc)
+				if len(product.ProductName) > 0 {
+					// convert the commerce.API struct into a database.Item
+					// so that it can be logged into the Pi client sqlite db
+					item := database.Item{
+						Index:           int64(i),
+						Barcode:         barcode,
+						Desc:            product.ProductName,
+						UserContributed: false}
+					pk, insertErr := item.Add(db, acc)
+					if insertErr == nil {
+						// also log the vendor/product code combination
+						database.AddVendorProduct(db, product.SKU, v.Id, pk)
+					}
+					productsFound += 1
 				}
 			}
+
+			if productsFound == 0 {
+				// add it to the Pi client sqlite db as "unknown"
+				// so that it can be manually edited/input
+				unknownItem := database.Item{Index: 0, Barcode: barcode}
+				unknownItem.Add(db, acc)
+			}
 		}
+
 		errorFn := func(e error) {
 			log.Fatal(e)
 		}
+
 		scanner.ScanForever(device, processScanFn, errorFn)
 	}
 }
