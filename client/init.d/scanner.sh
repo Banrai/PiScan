89ee254efcd408f2f861a7e70415bcddc85eb431
Denis Papathanasiou <denis@papathanasiou.org>
2015-01-02 17:44:11 -0500

    Added LSB-compliant headers at the start of both client init.d scripts

--- client/init.d/scanner.sh
+++ client/init.d/scanner.sh
@@ -1,4 +1,14 @@
 #! /bin/sh
+### BEGIN INIT INFO
+# Provides:          scanner.sh
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Runs the PiScanner binary
+# Description:       Makes sure the PiScanner binary starts on boot
+### END INIT INFO
+
 case "$1" in
   start)
     echo "Starting PiScanner"
@@ -9,7 +19,7 @@ case "$1" in
     killall PiScanner
     ;;
   *)
-    echo "Usage: /etc/init.d/scanner {start|stop}"
+    echo "Usage: /etc/init.d/scanner.sh {start|stop}"
     exit 1
     ;;
 esac
--- client/init.d/webapp.sh
+++ client/init.d/webapp.sh
@@ -1,4 +1,14 @@
 #! /bin/sh
+### BEGIN INIT INFO
+# Provides:          webapp.sh
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Runs the client WebApp binary
+# Description:       Makes sure the client WebApp binary starts on boot
+### END INIT INFO
+
 case "$1" in
   start)
     echo "Starting WebApp"
@@ -9,7 +19,7 @@ case "$1" in
     killall WebApp
     ;;
   *)
-    echo "Usage: /etc/init.d/webapp {start|stop}"
+    echo "Usage: /etc/init.d/webapp.sh {start|stop}"
     exit 1
     ;;
 esac
